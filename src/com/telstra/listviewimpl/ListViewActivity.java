2ccf1b760f6acd85f836d9d72826cc3c5867db4e
Nitin Sharma <nitin.sharma@itaas.com>
2014-08-18 12:12:44 +0530

    Developed basic layout for ListView and tested with dummy data.

--- src/com/telstra/listviewimpl/ListViewActivity.java
+++ src/com/telstra/listviewimpl/ListViewActivity.java
@@ -1,15 +1,26 @@
 package com.telstra.listviewimpl;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class ListViewActivity extends Activity {
+	private ArrayList<FeedElementInfo> mListFeed = new ArrayList<FeedElementInfo>();
+	final static String BLANK_STRING = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_view);
+
+		getFeedInfoList(null);
+
+		ListView lstView = (ListView) findViewById(R.id.lstView);
+		SimpleAdapter adapter = new SimpleAdapter(this, mListFeed);
+		lstView.setAdapter(adapter);
 	}
 
 	@Override
@@ -17,6 +28,40 @@ public class ListViewActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.list_view, menu);
 		return true;
+	}
+
+	public void getFeedInfoList(String url) {
+
+		// Sample list
+		mListFeed
+				.add(new FeedElementInfo(
+						"Sample Title Test....",
+						"kjfhsdefkjhsdfsd sdkhfdskjfhsdkfhdsfhsfjsf g sdfjgdsfdsg fdjs fgsdfg dsf hgsdjffjksfjsgfsd fFGSDJFKgdSfjf sfds dshgsdghdfghf",
+						"dsadadsad"));
+
+		mListFeed
+				.add(new FeedElementInfo(
+						"ssadsadsadsad",
+						"kjfhsdefkjhsdfsd sdkhfdskjfhsdkfhdsfhsfjsf g sdfjgdsfdsg fdjs fgsdfg dsf hgsdjffjksfjsgfsd fFGSDJFKgdSfjf sfds dshgsdghdfghf",
+						"dsadadsad"));
+		mListFeed
+				.add(new FeedElementInfo(
+						"ssadsadsadsad",
+						"kjfhsdefkjhsdfsd sdkhfdskjfhsdkfhdsfhsfjsf g sdfjgdsfdsg fdjs fgsdfg dsf hgsdjffjksfjsgfsd fFGSDJFKgdSfjf sfds dshgsdghdfghf",
+						"dsadadsad"));
+
+	}
+
+	public class FeedElementInfo {
+		public String mTitle;
+		public String mDescription;
+		public String mImgURL;
+
+		public FeedElementInfo(String title, String desc, String url) {
+			this.mTitle = title;
+			this.mDescription = desc;
+			this.mImgURL = url;
+		}
 	}
 
 }
---
+++ src/com/telstra/listviewimpl/SimpleAdapter.java
@@ -0,0 +1,49 @@
+package com.telstra.listviewimpl;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.telstra.listviewimpl.ListViewActivity.FeedElementInfo;
+
+public class SimpleAdapter extends ArrayAdapter<FeedElementInfo> {
+
+	ArrayList<FeedElementInfo> mListFeedInfo;
+	Activity mActivityContext;
+
+	public SimpleAdapter(Context context, ArrayList<FeedElementInfo> lstFeedInfo) {
+		super(context, R.layout.list_row_layout, lstFeedInfo);
+		mActivityContext = (Activity) context;
+		mListFeedInfo = lstFeedInfo;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View rowView = convertView;
+
+		if (rowView == null) {
+			LayoutInflater inflater = (LayoutInflater) mActivityContext
+					.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+			rowView = inflater.inflate(R.layout.list_row_layout, null);
+		}
+
+		TextView tvTitle = (TextView) rowView.findViewById(R.id.title);
+		TextView tvDesc = (TextView) rowView.findViewById(R.id.desc);
+		ImageView ivImage = (ImageView) rowView.findViewById(R.id.img);
+
+		FeedElementInfo feedInfo = mListFeedInfo.get(position);
+
+		tvTitle.setText((feedInfo.mTitle));
+		tvDesc.setText(feedInfo.mDescription);
+		ivImage.setImageResource(R.drawable.ic_launcher);
+
+		return rowView;
+	}
+}
